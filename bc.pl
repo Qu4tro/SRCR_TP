a17bd2e41a41a627478769434d4fa6d8ec69c50b
sixty65 <paradix65@gmail.com>
2017-03-17 16:21:24 +0000

    Base conhecimento inicial

---
+++ bc.pl
@@ -0,0 +1,38 @@
+
+
+
+%-------Base de Conhecimento----------
+% Extensao predicado utente(id, nome, idade, morada)
+
+utente(1, Xavier, , , ).
+utente(2, Patricia, , , ).
+utente(3, Carlos, , , ).
+utente(4, Daniel, , , ).
+utente(5, Marcia, , , ).
+utente(6, Andre, , , ).
+utente(7, Francisco, , , ).
+utente(8, Joao, , , ).
+utente(9, Diana, , , ).
+utente(10, Miguel, , , ).
+utente(11, Manuel, , , ).
+utente(12, Gil, , , ).
+utente(13, Sandra, , , ).
+utente(14, Adriana, , , ).
+utente(15, Tiago, , , ).
+utente(16, Fernando, , , ).
+utente(17, Celia, , , ).
+utente(18, Rui, , , ).
+utente(19, , , , ).
+utente(20, , , , ).
+
+
+
+
+
+
+
+
+
+
+
+
